429b99ae3c45c805295e979835a995f8182e71f6
Farhan <mansurifarhanfm@gmail.com>
2023-01-19 15:12:20 +0530

    g

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
+const bcrypt = require('bcrypt')
 const path = require('path')
+const Register = require('./models/Register')
 require('dotenv').config()
 app.use(
   cors({
@@ -18,4 +20,24 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname+'/routes/index.html'));
 });
+app.post("/register", (req,res)=>{
+  const newUser = new Register({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      newUser.password = hash;
+      newUser.save()
+      .then(user => {
+        res.json({ message: "User created successfully!" });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err });
+      });
+    }})
+})
 app.listen(3000, () => console.log(`listening on port ${3000}`));
