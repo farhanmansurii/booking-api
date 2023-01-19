55c5f7473e3517c012bd4ded675067f25ab67397
Farhan <mansurifarhanfm@gmail.com>
2023-01-19 13:30:58 +0530

    s

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const cors = require('cors')
 const app = express()
 
 require('dotenv').config()
- 
-app.use(express.json());
-
-
-
-app.use(cors({
-  origin: '*'
-}));
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());app.listen(5000, () => console.log("listening on port 5000"));
 
 mongoose.connect(process.env.MONGODB).then(()=>console.log("Database is connected"))
 
-app.use("/",(req,res)=>{
-  res.send("*********Welcome to booking API********")
-})
+
+app.get("/", (req, res) => {
+  res.send("Hi , Welcome to the Hotel API");
+});
+app.listen(3000, () => console.log("listening on port 5000"));
