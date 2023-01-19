073bb286af10afcc7c698c198fa932f9612f4fc7
Farhan <mansurifarhanfm@gmail.com>
2023-01-19 12:58:48 +0530

    NEW rep

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+
+require('dotenv').config()
+
+app.use(express.json());
+
+app.use(cors({
+  origin: '*'
+}));
+
+app.listen(process.env.PORT,()=>{
+  console.log(`${process.env.PORT} is listening `)
+})
+mongoose.connect(process.env.MONGODB).then(()=>console.log("Database is connected"))
+
+app.use("/",(req,res)=>{
+  res.send("*********Welcome to booking API********")
+})
