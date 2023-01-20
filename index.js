05706d19f632c4cd5d2c07b82e14d0a1014ee14f
farhan <mansurifarhanfm@gmail.com>
2023-01-21 02:51:24 +0530

    s

--- index.js
+++ index.js
@@ -1,63 +1,73 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
-const bcrypt = require('bcrypt')
-const path = require('path')
-const restaurantRouter = require('./controllers/Restaurant');
-const Register = require('./models/Register')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const bcrypt = require("bcrypt");
+const path = require("path");
+const restaurantRouter = require("./controllers/Restaurant");
+const Register = require("./models/Register");
+const Restaurant = require("./models/Restaurant");
+require("dotenv").config();
 app.use(
   cors({
     origin: "*",
   })
 );
-app.use(express.json())
-app.use('/restaurant', restaurantRouter);
+app.use(express.json());
+app.use("/restaurant", restaurantRouter);
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("connected to database :)");
 });
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname+'/routes/index.html'));
+app.get("/cuisines", (req, res) => {
+  Restaurant.distinct("cuisine", (err, cuisines) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).json(cuisines);
+    }
+  });
 });
-app.post('/login',(req,res)=>{
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/routes/index.html"));
+});
+app.post("/login", (req, res) => {
   Register.findOne({ email: req.body.email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
-      bcrypt.compare(req.body.password, user.password)
-        .then(isMatch => {
-          if (isMatch) {
-            return res.json({ message: "Success" });
-          } else {
-            return res.status(400).json({ message: "Incorrect password" });
-          }
-        });
+      bcrypt.compare(req.body.password, user.password).then((isMatch) => {
+        if (isMatch) {
+          return res.json({ message: "Success" });
+        } else {
+          return res.status(400).json({ message: "Incorrect password" });
+        }
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({ error: err });
     });
-})
-app.post("/register", (req,res)=>{
+});
+app.post("/register", (req, res) => {
   const newUser = new Register({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
   });
   bcrypt.hash(req.body.password, 10, (err, hash) => {
     if (err) {
       res.status(500).json({ error: err });
     } else {
       newUser.password = hash;
-      newUser.save()
-      .then(user => {
-        res.json({ message: `${newUser.username} created successfully` });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err });
-      });
-    }})
-})
+      newUser
+        .save()
+        .then((user) => {
+          res.json({ message: `${newUser.username} created successfully` });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err });
+        });
+    }
+  });
+});
 app.listen(3000, () => console.log(`listening on port ${3000}`));
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,81 +1,89 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const shortid = require('shortid');
-const slugify = require('slugify');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const shortid = require("shortid");
+const slugify = require("slugify");
 
 const restaurantSchema = new mongoose.Schema({
-    _id: {
-        type: String,
-        default: shortid.generate
+  _id: {
+    type: String,
+    default: shortid.generate,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: String,
+  address: {
+    type: String,
+  },
+  phone_number: String,
+  email: String,
+  website: String,
+  operating_hours: [
+    {
+      day: String,
+      open: String,
+      close: String,
     },
-    name: {
-        type: String,
-        required: true
+  ],
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  reviews: [
+    {
+      user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      text: String,
+      rating: Number,
     },
-    description: String,
-    address: {
-        type: String,
-
-    },
-    phone_number: String,
-    email: String,
-    website: String,
-    operating_hours: [{
-        day: String,
-        open: String,
-        close: String
-    }],
-    rating: {
-        type: Number,
-        min: 0,
-        max: 5
-    },
-    reviews: [{
-        user_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
+  ],
+  images: [String],
+  menus: [
+    {
+      name: String,
+      items: [
+        {
+          name: String,
+          description: String,
+          price: Number,
         },
-        text: String,
-        rating: Number
-    }],
-    "images": [String],
-    "menus": [{
-        "name": String,
-        "items": [{
-            "name": String,
-            "description": String,
-            "price": Number
-        }]
-    }],
-    "capacity": Number,
-    "location": {
-        "type": {
-            "type": String,
-            "default": "Point"
-        },
-        "coordinates": [Number]
+      ],
     },
-    "cuisine": String,
-    "price_range": Number,
-    "payment_methods": [String],
-    "amenities": [String],
-    "created_at": {
-        "type": Date,
-        "default": Date.now
+  ],
+  capacity: Number,
+  location: {
+    type: {
+      type: String,
+      default: "Point",
     },
-    "updated_at": {
-        "type": Date,
-        "default": Date.now
-    }
+    coordinates: [Number],
+  },
+  cuisine: [String],
+  price_range: Number,
+  payment_methods: [String],
+  amenities: [String],
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-restaurantSchema.pre('save', function(next) {
-    this._id = slugify(this.name, { lower: true });
-    next();
+restaurantSchema.pre("save", function (next) {
+  this._id = slugify(this.name, { lower: true });
+  next();
 });
 
 restaurantSchema.plugin(uniqueValidator);
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
 module.exports = Restaurant;
